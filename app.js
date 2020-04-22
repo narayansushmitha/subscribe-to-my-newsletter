755f7eac2cdee018cf7746b3a2d616e31d224b55
narayansushmitha <63325913+narayansushmitha@users.noreply.github.com>
2020-04-21 21:23:04 -0400

    updating styles

--- app.js
+++ app.js
@@ -50,8 +50,12 @@ app.post("/", function(req, res) {
 
   });
 
-  request.write(jsonData);
+  // request.write(jsonData);
   request.end();
+});
+
+app.post("/failure",function(req,res){
+  res.redirect("/");
 });
 
 app.listen(port);
