94030b205580d28a4852714727535da73a0fdca5
narayansushmitha <63325913+narayansushmitha@users.noreply.github.com>
2020-04-21 20:52:14 -0400

    adding success and failure jumbotrons

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const request = require("request");
+const https = require("https");
 
 const app = express();
 const port = '3000';
@@ -14,9 +15,43 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  var firstName = req.body.fname;
-  var lastName = req.body.lname;
-  var email = req.body.email;
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://us4.api.mailchimp.com/3.0/lists/d2eb1d7915";
+  const options = {
+    method: "POST",
+    auth: "sushmitha:381ed60f027d6311f8e5cfd68aa4c5bd-us4"
+  }
+
+  const request = https.request(url, options, function(response) {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(port);
@@ -24,5 +59,5 @@ app.listen(port);
 // Mailchimp api key
 // 381ed60f027d6311f8e5cfd68aa4c5bd-us4
 
-// list id 
+// list id
 // d2eb1d7915
